c726c3df958d9b2a91677f72753beca0daa147ba
Scott Sasaki <scottsasaki@gmail.com>
2023-10-13 17:25:35 -0700

    contact alignment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,16 @@ export default function Contact() {
   return (
     <section className="bg-pink" id="contact">
       <h2>Contact</h2>
-      <div className="glass text-center">
+      <div
+        className="glass text-center"
+        style={{ display: 'flex', flexDirection: 'column', gap: '1rem', alignItems: 'center' }}>
         <img src={headshot} alt="Cool looking guy" width="81" loading="lazy" />
 
         <div> If you would like to work together please send an email to</div>
-        <span className="name-first">SCOTT</span>
-        <span className="name-last">SASAKI</span>
-        <a href="mailto:scott@tacos.vegas">scott@tacos.vegas</a>
+
+        <div>
+          <a href="mailto:scott@tacos.vegas">scott@tacos.vegas</a>
+        </div>
       </div>
     </section>
   );
